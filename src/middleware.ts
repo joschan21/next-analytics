1c6523dc502ecbf497776f1261baff0f77a7759a
Paris Osuch <parisosuch.dev@gmail.com>
2024-02-23 17:06:00 -0800

    Await track() to successfully run serverless function

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { analytics } from './utils/analytics'
 export default async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/') {
     try {
-      analytics.track('pageview', {
+      await analytics.track('pageview', {
         page: '/',
         country: req.geo?.country,
       })
